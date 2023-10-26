229eec285fcd08a41768e6697910ae6d1180d03c
DhatriDwivedi <136470427+DhatriDwivedi@users.noreply.github.com>
2023-10-26 23:01:36 +0530

    src

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/LoginForm")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection=new mongoose.model('Collection1',LogInSchema)
+
+module.exports=collection
